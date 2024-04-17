b532a550365f920a8aaf74bf5842238f596c1613
Gunnidhi Sharma <114071149+Gunnidhis@users.noreply.github.com>
2024-04-17 22:00:16 +0530

    08.Infinite array_binary Search.py

---
+++ 8.Infinite array_binary Search.py
@@ -0,0 +1,82 @@
+# In this topic "Infinite array" we will do the Two questions :
+
+#       1: Position of element in infinite array
+#       2: index of first 1 in binary sorted array
+
+# which are not possible to come in coding round  because infinite array is not possible but it is
+# the possible that interviewer will ask someone to implement the binary search in infinite array
+
+# 1 Question : Position of  an element in infinite array
+
+# intuition - mid index is always between the start and end indices. 
+#             hence first we assume start = 0 end end = 1 
+#             increament the end index by 2 with multiplication while end index is less than target
+#             and simultaneously we change the start to end 
+#             in the last we will have the sequence i.e. start ----- mid ----- end
+#             now we will have the start and end pointer to implement the Binary Search as usual
+
+def Position_of_element_Binary_Search(nums,key: int)->int:
+    start = 0
+    end = 1
+    while nums[end] < key:
+        start = end
+        end *= 2
+        
+    while start <= end:
+        mid = start + (end - start)//2
+        if nums[mid] == key:
+            return mid
+        elif nums[mid] < key:
+            start = mid + 1
+        else:
+            end = mid -1
+    return -1
+
+     
+
+if __name__ == "__main__":
+    nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+    target = 7
+    ans = Position_of_element_Binary_Search(nums,target)
+    print(ans)
+
+
+# 2 Question : we have binary sorted array ( binary sorted array is defines as array which will have 
+#              only '0' and '1' and array is sorted i.e. all '0' are in first place and all '1' are in second place)
+#              we have to find the index of first 1 in infinite array
+
+
+# intuition - this question will be addition of two Questions(first occurance of element + infinite array concept using Binary Search)
+#                              1. firstly we adjust our mid according to infinite array concept
+#                              2. Apply first occurance concept in it i.e. if array[mid] == target ( in this case  target = 1 ) we will 
+#                                 continue our search because it might be a answer ( first occurance ) so store it move left side because
+#                                 lower index will be found in left side.
+
+
+
+
+def infinite_Binary_sorted_array(nums,target):
+    start = 0
+    end = 1
+    res = 1e5
+    while nums[end] < target:
+        start = end
+        end *= 2
+    
+    while start <= end :
+        mid = start + (end - start)//2
+        if nums[mid] == target:
+            res = min(res,mid)
+            end = mid - 1
+        elif nums[mid] > target:
+            end = mid - 1
+        elif nums[mid] < target:
+            start = mid + 1
+    return res
+
+    
+if __name__ == "__main__":
+    nums = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    target = 1
+    ans = infinite_Binary_sorted_array(nums,target)
+    print(ans)
